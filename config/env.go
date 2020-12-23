07a36c4c70e925a0cb928550d903663d92d6271a
unknown <csmcc2@hotmail.com>
2020-12-23 17:48:55 +0800

    env

--- config/env.go
+++ config/env.go
@@ -3,6 +3,9 @@ package config
 var mode int = 0
 
 // Deployment 部署模式 0: debug 1: release
-func Deployment() int {
-	return mode
+func Deployment() bool {
+	if mode == 0 {
+		return true
+	}
+	return false
 }
